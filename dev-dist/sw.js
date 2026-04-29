9c5b66b053f9487d3b98d022961c5e37209830cf
helenasf8 <helenadasilvafortunato.if@gmail.com>
2026-04-29 16:18:05 -0300

    feat: Upload de Imagens

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o3p5ddfcus"
+    "revision": "0.aij2hq6vc38"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
